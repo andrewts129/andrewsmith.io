30753a9859b312430daacdc96e69fa1bfedf8cd4
Andrew Smith <andrewts129@gmail.com>
2017-06-15 20:07:07 -0400

    First commit

---
+++ src/main/java/com/andrew/Andy.java
@@ -0,0 +1,12 @@
+package com.andrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Andy {
+
+	public static void main(String[] args) {
+        SpringApplication.run(Andy.class, args);
+    }
+}
