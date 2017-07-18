a2a224ee9e41cf5ba9b213d5ff1c6fec4003c26d
Andrew Smith <andrewts129@gmail.com>
2017-07-17 21:48:41 -0400

    package change

--- src/main/java/io/andrewsmith/Andy.java
+++ src/main/java/io/andrewsmith/Andy.java
@@ -1,4 +1,4 @@
-package com.andrew;
+package io.andrewsmith;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/io/andrewsmith/controllers/PageController.java
+++ src/main/java/io/andrewsmith/controllers/PageController.java
@@ -1,4 +1,4 @@
-package com.andrew.controllers;
+package io.andrewsmith.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
