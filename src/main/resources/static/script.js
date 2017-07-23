ef2b7d0ff3e7e2a015d7f20c665a8d0dff3c0b0e
Andrew Smith <andrewts129@gmail.com>
2017-07-22 21:33:46 -0400

    Not proud of this

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,36 +1,50 @@
 $(document).ready(function () {
-    setTimeout(beginFade, 3000);
 
-    var bg = $("#bg");
+    var text = $("#enter");
 
-    function beginFade() {
-        setTimeout(function(){
-            bg.removeClass("dark")}, 11000);
+    text.click(function() {
+        text.addClass("hide-text");
+        $("#sound-on").addClass("hide-text");
 
-        setTimeout(function () {
-            document.title = "The Imagine Dragons Appreciation Society";
-        }, 8000);
+        setTimeout(beginFade, 5000);
 
-        var sound = new Audio("radioactive.mp3");
-        sound.volume = 0.01;
-        sound.play();
-        setTimeout(increaseVolume, 1000);
-        setTimeout(danReynolds, 28000);
+        var bg = $("#night-visions");
+        var dan = $("#dan");
+        var shrug = $("#shrug");
 
-        function increaseVolume() {
-            sound.volume = sound.volume + 0.025;
+        function beginFade() {
+            setTimeout(function(){
+                bg.addClass("full-bright")}, 11000);
 
-            if (sound.volume < 0.97) {
-                setTimeout(increaseVolume, 1000);
+            setTimeout(function () {
+                document.title = "The Imagine Dragons Appreciation Society";
+            }, 8000);
+
+            var sound = new Audio("radioactive.mp3");
+            sound.volume = 0.01;
+            sound.play();
+            setTimeout(increaseVolume, 1000);
+            setTimeout(danReynolds, 28000);
+            setTimeout(fadeout, 161000);
+
+            function increaseVolume() {
+                sound.volume = sound.volume + 0.025;
+
+                if (sound.volume < 0.97) {
+                    setTimeout(increaseVolume, 1000);
+                }
+            }
+
+            function danReynolds() {
+                dan.addClass("mid-bright");
+            }
+
+            function fadeout() {
+                dan.addClass("dark");
+                bg.addClass("dark");
+                shrug.removeClass("dark");
+                shrug.addClass("full-bright");
             }
         }
-
-        function danReynolds() {
-            var dan = $("#dan");
-            dan.addClass("notdark");
-            dan.removeClass("dark");
-        }
-
-        setTimeout(function(){bg.addClass("dark")}, 160000);
-    }
+    });
 });
