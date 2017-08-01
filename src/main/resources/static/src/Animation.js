03bf5e17f83e935034c98a3f969e9e2eb3d4943b
Andrew Smith <andrewts129@gmail.com>
2017-07-31 21:57:40 -0400

    Added some animation

--- src/main/resources/static/src/Animation.js
+++ src/main/resources/static/src/Animation.js
@@ -6,4 +6,63 @@ export class Animation extends React.Component {
             <canvas id="main-canvas"></canvas>
         );
     }
+
+    componentDidMount() {
+        bgAnimation();
+    }
+}
+
+function bgAnimation() {
+    var canvas = document.getElementById("main-canvas");
+    var context = canvas.getContext("2d");
+
+    var circles = [];
+
+    var dt = 10;
+
+    function resizeCanvas() {
+        context.canvas.width = window.innerWidth;
+        context.canvas.height = window.innerHeight;
+    }
+
+    function Circle(x, y, radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+
+        this.color = "#327ffc";
+
+        this.update = function () {
+            this.radius = this.radius * 0.97;
+            this.draw()
+        };
+
+        this.draw = function () {
+            context.beginPath();
+            context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+            context.fillStyle = this.color;
+            context.stroke();
+        };
+    }
+
+    function updateAll() {
+        if (Math.random() > 0.90) {
+            var newCircle = new Circle(canvas.width * Math.random(), canvas.height * Math.random(), 30 * Math.random());
+            circles.push(newCircle);
+        }
+
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        for (var i = 0; i < circles.length; i++) {
+            circles[i].update();
+
+            if (circles[i].radius < 0.001) {
+                circles.splice(i, 1);
+            }
+        }
+
+        setTimeout(updateAll, dt);
+    }
+    resizeCanvas();
+    updateAll();
 }
