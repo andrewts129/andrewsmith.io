a8ed9f769c46f179081f58a3d838f038962e5008
Andrew Smith <andrewts129@gmail.com>
2017-08-10 11:03:23 -0400

    clarified some comments in js

--- src/main/resources/static/js/BackgroundAnimation.js
+++ src/main/resources/static/js/BackgroundAnimation.js
@@ -4,24 +4,32 @@ var context = canvas.getContext("2d");
 context.canvas.width = window.innerWidth;
 context.canvas.height = window.innerHeight;
 
+// How far off the screen the balls need to go before bouncing off the edges
+// If 0, they bounce off the edges of the screen
 var collisionPadding = 300;
 
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-const dt = 1;
-const G = 0.0001;
+const dt = 1;   // Number of milliseconds between each drawing/physics calculations
+const G = 0.0001;   // Big G, gravitational constant
 
+// This is the object that the balls gravitate around
 var blackHole = {
     x: width / 2,
     y: height / 2,
     mass: 40000000
 };
 
+// If the balls go faster than the max speed, their velocity gets multiplied by the below number to slow them down
 const maxSpeed = 10;
 const tooFastDampening = 0.2;
+
+// Balls' velocity gets multiplied by this when bouncing off something to prevent speeds from getting too crazy
+// It looks good too
 const bounceDampening = 0.6;
 
+// Creating the balls
 var allBalls = [];
 for (var i = 0; i < 15; i++) {
     var newBallX = randNumBetween(0, width);
@@ -35,12 +43,15 @@ for (var i = 0; i < 15; i++) {
     allBalls.push(newBall);
 }
 
+// This method calls itself again when it's done, so it runs forever basically
 updateAll();
 
+// Generates a random decimal number between a and b.
 function randNumBetween(a, b) {
     return Math.random() * (b - a) + a
 }
 
+// Generates a random rgba color that is mostly red and pretty transparent
 function generateColor() {
     var redValue = Math.floor(randNumBetween(140, 220));
     var opacity = randNumBetween(0.1, 0.5);
@@ -51,6 +62,7 @@ function generateColor() {
 function updateAll() {
     context.clearRect(0, 0, width, height);
 
+    // Do the physics calculations for each ball and draw them
     for (var i = 0; i < allBalls.length; i++) {
         var ball = allBalls[i];
         ball.update();
@@ -63,7 +75,7 @@ function Ball(x, y, radius, color) {
     var self = this;
 
     this.radius = radius;
-    this.mass = (4/3) * Math.PI * Math.pow(self.radius, 3) / 1000;
+    this.mass = (4/3) * Math.PI * Math.pow(self.radius, 3) / 1000;  // Divide by 1000 bc the balls were way too heavy
     this.color = color;
 
     this.x = x;
@@ -87,6 +99,7 @@ function Ball(x, y, radius, color) {
         self.v_x = self.v_x + (self.a_x * dt);
         self.v_y = self.v_y + (self.a_y * dt);
 
+        // Slow the ball down if it's going too fast
         if (self.v_x > maxSpeed || self.v_x < -maxSpeed) {
             self.v_x = self.v_x * tooFastDampening;
         }
@@ -105,19 +118,23 @@ function Ball(x, y, radius, color) {
 
         // To prevent weird behavior when the ball gets really close to the center
         if (distBetweenSq > 5000) {
+
+            // Magnitude of the gravitational force vector
             var fMagnitude = G * self.mass * blackHole.mass / distBetweenSq;
 
-            var unitVectorTowardsCenter = {
+            // Take that vector and point it towards the black hole
+            var unitVectorTowardsHole = {
                 x: (blackHole.x - self.x) / Math.sqrt(distBetweenSq),
                 y: (blackHole.y - self.y) / Math.sqrt(distBetweenSq)
             };
-
-            self.f_x = fMagnitude * unitVectorTowardsCenter.x;
-            self.f_y = fMagnitude * unitVectorTowardsCenter.y;
+            self.f_x = fMagnitude * unitVectorTowardsHole.x;
+            self.f_y = fMagnitude * unitVectorTowardsHole.y;
         }
     }
 
     function checkCollisionBorders() {
+        // Reverses the ball's direction if it hits a boundary (usually the boundaries are pretty far outside of the
+        // screen
         if ((self.x - self.radius + collisionPadding < 0) || (self.x + self.radius - collisionPadding > width)) {
             self.v_x = -self.v_x * bounceDampening;
         }
@@ -128,10 +145,12 @@ function Ball(x, y, radius, color) {
     }
 
     function draw() {
+        // Returns true if the ball is currently on the screen
         function isVisible() {
             return !(self.x + self.radius < 0 || self.x - self.radius > width || self.y + self.radius < 0 || self.y - self.radius > height);
         }
 
+        // Only draw the ball if it's actually visible
         if (isVisible()) {
             context.beginPath();
             context.arc(self.x, self.y, self.radius, 0, 2 * Math.PI);
