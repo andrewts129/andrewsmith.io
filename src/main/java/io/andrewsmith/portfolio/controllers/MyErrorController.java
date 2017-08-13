ea5796d900b86e8846a1063a10c8953054ce610d
andrewts129 <andrewts129@gmail.com>
2017-08-13 13:09:25 -0400

    made error page generic

--- src/main/java/io/andrewsmith/portfolio/controllers/MyErrorController.java
+++ src/main/java/io/andrewsmith/portfolio/controllers/MyErrorController.java
@@ -2,18 +2,12 @@ package io.andrewsmith.portfolio.controllers;
 
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyErrorController implements ErrorController {
 
     @Override
     public String getErrorPath() {
-        return "/error2";
-    }
-
-    @RequestMapping(value = "/error2", produces = "text/html")
-    public String errorPage() {
-        return "error.html";
+        return "/error";
     }
 }
