813d486701992d20e1884b3c60b3d7559cf9da8f
andrewts129 <andrewts129@gmail.com>
2017-08-13 12:44:57 -0400

    added error page

--- src/main/java/io/andrewsmith/portfolio/controllers/FileController.java
+++ src/main/java/io/andrewsmith/portfolio/controllers/FileController.java
@@ -5,15 +5,15 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
-@Controller
+@RestController
 public class FileController {
 
-    @RequestMapping("/resume")
+    @RequestMapping(value = "/resume", produces = "application/pdf")
     public ResponseEntity<InputStreamResource> getResume() throws IOException {
         ClassPathResource resumePdfFile = new ClassPathResource("files/AndrewSmithResume.pdf");
 
