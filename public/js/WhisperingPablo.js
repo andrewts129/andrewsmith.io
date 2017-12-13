879fc7dac65a8afe6a67a0be4d0f18eaafc0f7df
Andrew Smith <andrewts129@gmail.com>
2017-12-12 22:49:29 -0500

    made feed functional

--- public/js/WhisperingPablo.js
+++ public/js/WhisperingPablo.js
@@ -1,10 +1,14 @@
 window.onload = function() {
+    // The intervals are chosen so that they will rarely occur at the same time. This probably does not matter at all
     updateWhisperCount();
-    setInterval(updateWhisperCount, 3000);
+    setInterval(updateWhisperCount, 2237);
+
+    updateFeed();
+    setInterval(updateFeed, 1179);
 };
 
 function updateWhisperCount() {
-    var url = "https://whispering-pablo.herokuapp.com/api/numsubmissions";
+    var url = "https://whispering-pablo.herokuapp.com/api/get/numsubmissions";
 
     window.fetch(url, {
         method: "get"
@@ -12,8 +16,9 @@ function updateWhisperCount() {
         return response.json();
     }).then(function (json) {
         document.getElementById("num-whispers").innerText = json.number;
-        document.getElementById("output-label").innerText = "The last message submitted was: ";
+        document.getElementById("connecting-label").innerText = "";
     });
+
 }
 
 function submitMessage() {
@@ -23,7 +28,7 @@ function submitMessage() {
     if (isValidSubmission(input)) {
         inputTextField.value = "";
 
-        var url = "https://whispering-pablo.herokuapp.com/api/postmessage?request=" + input;
+        var url = "https://whispering-pablo.herokuapp.com/api/post/submit?message=" + input;
 
         window.fetch(url, {
             method: "post"
@@ -35,6 +40,64 @@ function submitMessage() {
     }
 
     updateWhisperCount();
+    updateFeed();
+}
+
+function updateFeed() {
+    function renderFeed(feedJson) {
+        var feedHolder = document.getElementById("feed");
+
+        // Clears children
+        // https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+        while (feedHolder.lastChild) {
+            feedHolder.removeChild(feedHolder.lastChild);
+        }
+
+        for (var i = 0; i < feedJson.length; i++) {
+            var creationTime = Date.parse(feedJson[i].creation_time);
+            var deletionTime = Date.parse(feedJson[i].deletion_time);
+
+            var now = Math.floor((new Date()).getTime());
+            var opacity =  1 - (now - creationTime) / (deletionTime - creationTime);
+
+            if (opacity > 1) {
+                // should never happen assume the laws of time stay sane
+                opacity = 1;
+            }
+            // will also probably never happen, but still don't attempt to render negative opacity
+            else if (opacity > 0) {
+                var feedElement = document.createElement("div");
+                feedElement.className = "feed-element";
+
+                var feedElementText = document.createElement("h4");
+                feedElementText.className = "feed-element-text";
+                feedElementText.innerText = feedJson[i].message;
+                feedElementText.style.opacity = opacity;
+
+                feedElement.appendChild(feedElementText);
+
+                feedHolder.appendChild(feedElement);
+
+            }
+        }
+    }
+
+    var url = "https://whispering-pablo.herokuapp.com/api/get/feed";
+
+    window.fetch(url, {
+        method: "get"
+    }).then(function (response) {
+        return response.json();
+    }).then(function (json) {
+        renderFeed(json);
+    });
+}
+
+// So that the count increments immediately upon hitting submit. Does not change anything server-side
+function incrementWhisperCount() {
+    var numWhispersElement = document.getElementById("num-whispers");
+    var currentCount = numWhispersElement.innerText;
+    numWhispersElement.innerText = parseInt(currentCount, 10) + 1;
 }
 
 function isValidSubmission(text) {
@@ -49,5 +112,6 @@ function isValidSubmission(text) {
         return true;
     }
 
-    return text.length > 0 && !stringIsAllSpaces(text);
+    return text.length > 0 && text.length <= 500 && !stringIsAllSpaces(text);
 }
+
