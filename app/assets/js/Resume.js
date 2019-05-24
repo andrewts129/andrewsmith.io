1c8ae305f0d42af623fc51b8a90d22a7cf65fe6f
Andrew Smith <andrewts129@gmail.com>
2019-05-24 15:35:52 -0400

    resume refactoring

--- app/assets/js/Resume.js
+++ app/assets/js/Resume.js
@@ -1,5 +1,5 @@
-setTimeout(function () {
+function redirectToPdf() {
     const urlParams = new URLSearchParams(window.location.search);
     const b = urlParams.has("b") ? urlParams.get("b") : "1";
-    window.location.replace("http://www.andrewsmith.io/AndrewSmithResume?b=" + b)
-}, 250);
+    window.location.replace(window.location.host + "/AndrewSmithResume?b=" + b)
+}
--- app/controllers/FileController.scala
+++ app/controllers/FileController.scala
@@ -12,10 +12,10 @@ import scalaj.http.{Http, HttpRequest, HttpResponse}
 
 @Singleton
 class FileController @Inject()(cc: ControllerComponents, ws: WSClient) extends AbstractController(cc) {
-    def getResume(b: Option[String]): Action[AnyContent] = Action {
+    def resume(b: Option[String]): Action[AnyContent] = Action {
         def downloadResume(branch: String): Array[Byte] = {
             val url: String = "https://raw.githubusercontent.com/andrewts129/resume/" + branch + "/AndrewSmithResume.pdf"
-            val authToken: String = sys.env("GITHUB_ACCESS_TOKEN")
+            val authToken: String = "4741b451efc74e01699291a39c37b6f325389ad1"
 
             val request: HttpRequest = Http(url).header("Authorization", "token " + authToken)
             val response: HttpResponse[Array[Byte]] = request.asBytes
@@ -26,9 +26,6 @@ class FileController @Inject()(cc: ControllerComponents, ws: WSClient) extends A
         val branches: Map[String, String] = Map("0" -> "master", "1" -> "trump").withDefaultValue("trump")
         val resumeBytes: Array[Byte] = downloadResume(branches(b.getOrElse("1")))
 
-        Result(
-            header = ResponseHeader(200, Map.empty),
-            body = HttpEntity.Strict(ByteString(resumeBytes), Some("application/pdf"))
-        )
+        Ok(resumeBytes).as("application/pdf")
     }
 }
