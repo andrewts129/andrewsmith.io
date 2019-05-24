ebf547f4c863c75f881705b3ed70f9c4899f0651
Andrew Smith <andrewts129@gmail.com>
2019-05-24 18:06:16 -0400

    removed accidental github token

--- app/controllers/FileController.scala
+++ app/controllers/FileController.scala
@@ -15,7 +15,7 @@ class FileController @Inject()(cc: ControllerComponents, ws: WSClient) extends A
     def resume(b: Option[String]): Action[AnyContent] = Action {
         def downloadResume(branch: String): Array[Byte] = {
             val url: String = "https://raw.githubusercontent.com/andrewts129/resume/" + branch + "/AndrewSmithResume.pdf"
-            val authToken: String = "4741b451efc74e01699291a39c37b6f325389ad1"
+            val authToken: String = sys.env("GITHUB_ACCESS_TOKEN")
 
             val request: HttpRequest = Http(url).header("Authorization", "token " + authToken)
             val response: HttpResponse[Array[Byte]] = request.asBytes
