8307195d46d7dc229e23a2ef1d577266ebb22742
Andrew Smith <andrewts129@gmail.com>
2019-05-24 18:03:34 -0400

    fixed redirect page

--- app/assets/js/Resume.js
+++ app/assets/js/Resume.js
@@ -1,5 +1,5 @@
 function redirectToPdf() {
     const urlParams = new URLSearchParams(window.location.search);
     const b = urlParams.has("b") ? urlParams.get("b") : "1";
-    window.location.replace(window.location.host + "/AndrewSmithResume?b=" + b)
+    window.location.replace("http://" + window.location.host + "/AndrewSmithResume?b=" + b)
 }
