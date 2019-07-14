f8501bc7fceff45581455cf302109481e178507d
Andrew Smith <andrewts129@gmail.com>
2019-07-14 17:01:13 -0400

    changed default branch for resume again

--- app/assets/js/Resume.js
+++ app/assets/js/Resume.js
@@ -1,5 +1,5 @@
 function redirectToPdf() {
     const urlParams = new URLSearchParams(window.location.search);
-    const b = urlParams.has("b") ? urlParams.get("b") : "1";
+    const b = urlParams.has("b") ? urlParams.get("b") : "0";
     window.location.replace("http://" + window.location.host + "/AndrewSmithResume?b=" + b)
 }
