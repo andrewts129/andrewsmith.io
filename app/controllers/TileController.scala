30c46e58acc9d09f90273be30de2c3ee2d938b2b
Andrew Smith <andrewts129@gmail.com>
2019-07-21 14:55:47 -0400

    added caching

--- app/controllers/TileController.scala
+++ app/controllers/TileController.scala
@@ -1,25 +1,29 @@
 package controllers
 
 import javax.inject.{Inject, Singleton}
+import play.api.cache.Cached
 import play.api.libs.ws.WSClient
-import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
-
+import play.api.mvc.{AbstractController, ControllerComponents, EssentialAction}
 import scalaj.http.{Http, HttpRequest}
 
 
 @Singleton
-class TileController @Inject()(cc: ControllerComponents, ws: WSClient) extends AbstractController(cc) {
+class TileController @Inject()(cc: ControllerComponents, ws: WSClient, cached: Cached) extends AbstractController(cc) {
     private val baseUrl: String = "https://columbus-building-tileserver.herokuapp.com"
 
-    def getStyle(name: String): Action[AnyContent] = Action {
-        val url: String = s"$baseUrl/styles/$name/style.json"
-        val request: HttpRequest = Http(url)
-        Ok(request.asBytes.body)
+    def getStyle(name: String): EssentialAction = cached(s"style_$name") {
+        Action {
+            val url: String = s"$baseUrl/styles/$name/style.json"
+            val request: HttpRequest = Http(url)
+            Ok(request.asBytes.body)
+        }
     }
 
-    def getTile(source: String, z: String, y: String, x: String): Action[AnyContent] = Action {
-        val url: String = s"$baseUrl/data/$source/$z/$y/$x.pbf"
-        val request: HttpRequest = Http(url)
-        Ok(request.asBytes.body)
+    def getTile(source: String, z: String, y: String, x: String): EssentialAction = cached(s"tile_${source}_${z}_${y}_$x") {
+        Action {
+            val url: String = s"$baseUrl/data/$source/$z/$y/$x.pbf"
+            val request: HttpRequest = Http(url)
+            Ok(request.asBytes.body)
+        }
     }
 }
