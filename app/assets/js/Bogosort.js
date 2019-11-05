5ee51adcd7c972f5244cc1f74087c0e114568582
Andrew Smith <andrewts129@gmail.com>
2019-11-05 14:59:43 -0500

    some timing info

--- app/assets/js/Bogosort.js
+++ app/assets/js/Bogosort.js
@@ -4,9 +4,9 @@ window.onload = async function () {
         return response.json()
     }
 
-    const startDate = new Date(2019, 11, 4, 18, 27);
+    const startDate = new Date(2019, 10, 4, 18, 27);
 
-    let currentData = await fetchData();
+    let currentData = await fetchData(true);
 
     const margin = {top: 20, right: 20, bottom: 20, left: 20};
     const width = 750 - margin.left - margin.right;
@@ -27,13 +27,56 @@ window.onload = async function () {
         .append("g")
         .attr("translate", "translate(" + margin.left + ", " + margin.top + ")");
 
+    const t = svg.transition().duration(500);
+
+    const getDurationString = (start, end) => {
+        const diff = new Date(end - start);
+        let result = "";
+
+        if (diff.getSeconds() === 1) {
+            result = "1 second"
+        } else {
+            result = "" + diff.getSeconds() + " seconds"
+        }
+
+        if (diff.getMinutes() === 1) {
+            result = "1 minute and " + result
+        } else if (diff.getMinutes() > 1) {
+            result = "" + diff.getMinutes() + " minutes and " + result
+        }
+
+        if (diff.getHours() === 1) {
+            result = "1 hour, " + result
+        } else if (diff.getHours() > 1) {
+            result = "" + diff.getHours() + " hours, " + result
+        }
+
+        if (diff.getDate() === 1) {
+            result = "1 day, " + result
+        } else if (diff.getDate() > 1) {
+            result = "" + diff.getDate() + " days, " + result
+        }
+
+        if (diff.getMonth() === 1) {
+            result = "1 month, " + result
+        } else if (diff.getMonth() > 1) {
+            result = "" + diff.getMonth() + " months, " + result
+        }
+
+        if (diff.getFullYear() === 1) {
+            result = "1 year, " + result
+        } else if (diff.getMonth() > 1) {
+            result = "" + diff.getMonth() + " years, " + result
+        }
+
+        return result
+    };
 
     const redraw = async () => {
-        currentData = await fetchData();
-        const t = svg.transition().duration(500);
+        const newData = await fetchData();
 
         svg.selectAll("rect")
-            .data(currentData.array, (d) => d)
+            .data(newData.array, (d) => d)
             .join(
                 (enter) =>
                     enter.append("rect")
@@ -47,6 +90,16 @@ window.onload = async function () {
             .attr("y", (d) => y(d))
             .attr("width", x.bandwidth())
             .attr("height", (d) => y(0) - y(d));
+
+        if (newData.numCompletions === 1) {
+            document.getElementById("num-completions").innerText = "" + newData.numCompletions + " time"
+        } else {
+            document.getElementById("num-completions").innerText = "" + newData.numCompletions + " times"
+        }
+
+        document.getElementById("total-duration").innerText = getDurationString(startDate, new Date());
+
+        currentData = newData;
     };
 
     await redraw();
