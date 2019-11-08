86eea392b280d3c757472c1a1cd615296f06532e
Andrew Smith <andrewts129@gmail.com>
2019-11-08 12:54:39 -0500

    added quill

--- app/controllers/BogoController.scala
+++ app/controllers/BogoController.scala
@@ -1,17 +1,29 @@
 package controllers
 
+import io.getquill._
 import javax.inject.{Inject, Singleton}
 import play.api.libs.json.Json
 import play.api.libs.ws.WSClient
 import play.api.mvc._
 import utils.Bogosorter
 
+case class BogoStats(key: String, value: String)
+
 @Singleton
 class BogoController @Inject()(cc: ControllerComponents, ws: WSClient) extends AbstractController(cc) {
   def data: Action[AnyContent] = Action {
+    lazy val ctx = new PostgresJdbcContext(SnakeCase, "ctx")
+    import ctx._
+
+    val q = quote {
+      query[BogoStats].filter(stat => stat.key == "numCompletions").map(stat => stat.value).take(1)
+    }
+
+    val numCompletions: String = ctx.run(q).head
+
     Ok(Json.obj(
       "array" -> Bogosorter.array,
-      "numCompletions" -> Bogosorter.numCompletions,
+      "numCompletions" -> numCompletions.toInt,
       "lastCompletion" -> "todo")
     )
   }
