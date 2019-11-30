ab56098364d5ef3de7962c16a952a80c55e998fc
Andrew Smith <andrewts129@gmail.com>
2019-11-30 17:21:47 -0500

    comments

--- app/models/BogoStats.scala
+++ app/models/BogoStats.scala
@@ -3,7 +3,8 @@ package models
 import io.getquill.{PostgresJdbcContext, SnakeCase}
 
 object BogoStats {
-  // An RDBMS is definitely not the best solution here. But I already have one running on the server so... ‾\_(ツ)_/‾
+  // An RDBMS is definitely not the best solution here. But it works and I already have one running on the
+  // server so... ‾\_(ツ)_/‾
   private case class BogoStats(key: String, value: String)
 
   lazy private val ctx = new PostgresJdbcContext(SnakeCase, "ctx")
@@ -20,7 +21,7 @@ object BogoStats {
     }
 
     def +=(n: Int): Unit = {
-      // TODO figure out how to do this in one query... had trouble putting too much logic in the UPDATE query
+      // TODO figure out how to do this in one query... had trouble putting .toString() logic in the UPDATE query
       import ctx._
 
       val newValue: String = (NumCompletions() + n).toString
