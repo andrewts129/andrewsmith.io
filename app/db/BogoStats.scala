81358d6c232fab834ed7330433d83ccabfb1755d
Andrew Smith <andrewts129@gmail.com>
2019-11-30 18:44:46 -0500

    removed level zero constraint on swaydb

--- app/db/BogoStats.scala
+++ app/db/BogoStats.scala
@@ -4,7 +4,7 @@ import swaydb._
 import swaydb.serializers.Default._
 
 object BogoStats {
-  private val stats = swaydb.persistent.zero.Map[String, Int, Nothing, IO.ApiIO](dir = ".swaydb/bogostats").get
+  private val stats = swaydb.persistent.Map[String, Int, Nothing, IO.ApiIO](dir = ".swaydb/bogostats").get
 
   object NumCompletions {
     def get: Int = {
