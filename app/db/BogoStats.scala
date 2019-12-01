9e8ed4bf0ad4b9945ed67d73e6769d900e3d4dc5
Andrew Smith <andrewts129@gmail.com>
2019-11-30 19:14:27 -0500

    made add operation atomic

--- app/db/BogoStats.scala
+++ app/db/BogoStats.scala
@@ -7,12 +7,19 @@ object BogoStats {
   private val stats = swaydb.persistent.Map[String, Int, Nothing, IO.ApiIO](dir = ".swaydb/bogostats").get
 
   object NumCompletions {
+    if (!stats.contains("numCompletions").get) {
+      stats.put("numCompletions", 0)
+    }
+
     def get: Int = {
-      stats.get("numCompletions").get.getOrElse(0)
+      stats.get("numCompletions").get.get
     }
 
     def add(x: Int): Unit = {
-      stats.put("numCompletions", NumCompletions.get + x)
+      stats.map {
+        case ("numCompletions", value) => ("numCompletions", value + x)
+        case (key, value) => (key, value)
+      }.materialize.flatMap(stats.put).get
     }
   }
 }
