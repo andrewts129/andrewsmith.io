5224830804bedb3f156c08476ab7eb62e141b9e1
Andrew Smith <andrewts129@gmail.com>
2020-01-08 21:27:48 -0500

    Twilio webhook compatibility

--- app/controllers/MessageQueueController.scala
+++ app/controllers/MessageQueueController.scala
@@ -8,12 +8,21 @@ import play.api.mvc.{AbstractController, Action, AnyContent, AnyContentAsFormUrl
 @Singleton
 class MessageQueueController @Inject()(cc: ControllerComponents, ws: WSClient) extends AbstractController(cc) {
   def add: Action[AnyContent] = Action { request =>
-    val body: String = request.body match {
-      case AnyContentAsText(text) => text
-      case AnyContentAsFormUrlEncoded(data) => data("message").head
+    val body: Option[String] = request.body match {
+      case AnyContentAsText(text) => Some(text)
+      case AnyContentAsFormUrlEncoded(data) => data.get("Body") match {
+        case Some(value) => Some(value.head)
+        case _ => None
+      }
+      case _ => None
     }
-    MessageQueue.add(body)
-    Created(body)
+
+    body match {
+      case Some(value) =>
+        MessageQueue.add(value)
+        Accepted
+      case None => BadRequest
+    }
   }
 
   def pop: Action[AnyContent] = Action {
