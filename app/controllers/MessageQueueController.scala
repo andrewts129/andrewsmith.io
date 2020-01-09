9244f3408bba3484ff47d765554555282eabe7e4
Andrew Smith <andrewts129@gmail.com>
2020-01-08 20:05:16 -0500

    made message add more flexible

--- app/controllers/MessageQueueController.scala
+++ app/controllers/MessageQueueController.scala
@@ -3,12 +3,15 @@ package controllers
 import db.MessageQueue
 import javax.inject.{Inject, Singleton}
 import play.api.libs.ws.WSClient
-import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents}
+import play.api.mvc.{AbstractController, Action, AnyContent, AnyContentAsFormUrlEncoded, AnyContentAsText, ControllerComponents}
 
 @Singleton
 class MessageQueueController @Inject()(cc: ControllerComponents, ws: WSClient) extends AbstractController(cc) {
   def add: Action[AnyContent] = Action { request =>
-    val body = request.body.asFormUrlEncoded.get("message").head  // TODO fix this
+    val body: String = request.body match {
+      case AnyContentAsText(text) => text
+      case AnyContentAsFormUrlEncoded(data) => data("message").head
+    }
     MessageQueue.add(body)
     Created(body)
   }
