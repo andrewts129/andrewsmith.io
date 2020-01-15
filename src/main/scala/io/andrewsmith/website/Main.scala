7f3a60b5ec56df643cb7f36de8c4583ce223cb50
Andrew Smith <andrewts129@gmail.com>
2020-01-14 22:54:58 -0500

    removed routes object

--- src/main/scala/io/andrewsmith/website/Main.scala
+++ src/main/scala/io/andrewsmith/website/Main.scala
@@ -2,13 +2,24 @@ package io.andrewsmith.website
 
 import cats.effect._
 import cats.implicits._
-import io.andrewsmith.website.services.Routes
+import io.andrewsmith.website.services.{ResourceService, RestService, ViewService}
+import org.http4s.HttpApp
+import org.http4s.implicits._
+import org.http4s.server.Router
 import org.http4s.server.blaze._
 
+
 object Main extends IOApp {
+  // TODO make robots.txt accessible at /robots.txt
+  private val app: HttpApp[IO] = Router(
+    "/" -> ViewService.routes,
+    "/api" -> RestService.routes,
+    "/assets" -> ResourceService.routes
+  ).orNotFound
+
   override def run(args: List[String]): IO[ExitCode] = BlazeServerBuilder[IO]
     .bindHttp(4000)
-    .withHttpApp(Routes.httpApp)
+    .withHttpApp(app)
     .serve
     .compile
     .drain
