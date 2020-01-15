2d127b373fa766bcb88e6d6c0cb8e8a36f03f93d
Andrew Smith <andrewts129@gmail.com>
2020-01-15 13:52:12 -0500

    filled in .io.net  html

--- src/main/scala/io/andrewsmith/website/views/Home.scala
+++ src/main/scala/io/andrewsmith/website/views/Home.scala
@@ -7,16 +7,23 @@ import scalatags.Text.implicits._
 import scalatags.Text.tags._
 import scalatags.Text.tags2.title
 
-// TODO implement
+// TODO
 object Home {
   val page: TypedTag[String] = html(
     head(
       meta(charset := "UTF-8"),
-      title("TODO"),
+      title("andrewsmith.io.net"),
       link(rel := "icon", `type` := "image/png", href := "/assets/images/favicon.png")
     ),
     body(
-      h1("TODO")
+      h1("welcome to www.andrewsmith.io.net!"),
+      form(method := "get", action := "https://www.google.com/search",
+        label(`for` := "q",
+          span("Google Search:"),
+          input(`type` := "text", name := "q"),
+          button(`type` := "submit", "Search")
+        )
+      )
     ),
     GoogleAnalytics.scriptTag
   )
