47505586478161cf46d5d0b51a032a1e1d6bab20
Andrew Smith <andrewts129@gmail.com>
2020-01-15 13:12:59 -0500

    filled in bogosort view

--- src/main/scala/io/andrewsmith/website/views/Bogosort.scala
+++ src/main/scala/io/andrewsmith/website/views/Bogosort.scala
@@ -6,15 +6,21 @@ import scalatags.Text.implicits._
 import scalatags.Text.tags._
 import scalatags.Text.tags2.title
 
-// TODO implement
+// TODO
 object Bogosort {
   val page: TypedTag[String] = html(
     head(
-      title("TODO"),
+      title("Bogosort"),
       link(rel := "icon", `type` := "image/png", href := "/assets/images/favicon.png")
     ),
     body(
-      h1("TODO")
-    )
+      h1("Bogosort"),
+      div(id := "bogo-container"),
+      p(s"This program has been running for ",
+        span(id := "total-duration"),
+        " and has sorted the array ",
+        span(id := "num-completions"), ".")
+    ),
+    script(`type` := "text/javascript", src := "https://d3js.org/d3.v5.min.js")
   )
 }
