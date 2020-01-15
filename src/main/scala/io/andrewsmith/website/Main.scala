58790042be74dee08fe69a32a596b864541f3215
Andrew Smith <andrewts129@gmail.com>
2020-01-14 23:16:52 -0500

    added FileService

--- src/main/scala/io/andrewsmith/website/Main.scala
+++ src/main/scala/io/andrewsmith/website/Main.scala
@@ -2,7 +2,7 @@ package io.andrewsmith.website
 
 import cats.effect._
 import cats.implicits._
-import io.andrewsmith.website.services.{ResourceService, RestService, ViewService}
+import io.andrewsmith.website.services.{FileService, ResourceService, RestService, ViewService}
 import org.http4s.HttpApp
 import org.http4s.implicits._
 import org.http4s.server.Router
@@ -10,9 +10,8 @@ import org.http4s.server.blaze._
 
 
 object Main extends IOApp {
-  // TODO make robots.txt accessible at /robots.txt
   private val app: HttpApp[IO] = Router(
-    "/" -> ViewService.routes,
+    "/" -> FileService.routes.combineK(ViewService.routes),
     "/api" -> RestService.routes,
     "/assets" -> ResourceService.routes
   ).orNotFound
---
+++ src/main/scala/io/andrewsmith/website/services/FileService.scala
@@ -0,0 +1,12 @@
+package io.andrewsmith.website.services
+
+import cats.effect.IO
+import org.http4s.HttpRoutes
+import org.http4s.dsl.io._
+
+object FileService {
+  val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
+    case GET -> Root / "robots.txt" => Ok("ROBOTS")
+    case GET -> Root / "AndrewSmithResume.pdf" => Ok("TODO resume")
+  }
+}
