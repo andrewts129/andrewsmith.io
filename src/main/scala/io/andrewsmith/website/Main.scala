dff5585f6411c1ce1e314ebb23e0dfbcec632120
Andrew Smith <andrewts129@gmail.com>
2020-01-16 11:28:40 -0500

    separated rest service into parts

--- src/main/scala/io/andrewsmith/website/Main.scala
+++ src/main/scala/io/andrewsmith/website/Main.scala
@@ -2,7 +2,7 @@ package io.andrewsmith.website
 
 import cats.effect._
 import cats.implicits._
-import io.andrewsmith.website.services.{FileService, ResourceService, RestService, ViewService}
+import io.andrewsmith.website.services._
 import org.http4s.HttpApp
 import org.http4s.implicits._
 import org.http4s.server.Router
@@ -12,7 +12,7 @@ import org.http4s.server.blaze._
 object Main extends IOApp {
   private val app: HttpApp[IO] = Router(
     "/" -> FileService.routes.combineK(ViewService.routes),
-    "/api" -> RestService.routes,
+    "/api" -> BogosortApiService.routes.combineK(MessagesApiService.routes),
     "/assets" -> ResourceService.routes
   ).orNotFound
 
--- src/main/scala/io/andrewsmith/website/services/MessagesApiService.scala
+++ src/main/scala/io/andrewsmith/website/services/MessagesApiService.scala
@@ -1,22 +1,15 @@
 package io.andrewsmith.website.services
 
 import cats.effect.IO
-import io.andrewsmith.website.db.{BogoStats, Message}
+import io.andrewsmith.website.db.Message
 import io.circe.generic.auto._
 import io.circe.syntax._
 import org.http4s.HttpRoutes
 import org.http4s.circe._
 import org.http4s.dsl.io._
 
-object RestService {
-  case class BogosortState(array: Seq[Int], numCompletions: Int)
-
+object MessagesApiService {
   val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
-    case GET -> Root / "bogosort" / "data" =>
-      val array = Seq(1, 2, 3) // TODO
-      BogoStats.numCompletions.flatMap(n =>
-        Ok(BogosortState(array, n).asJson)
-      )
     case POST -> Root / "messages" / "add" =>
       Message.add("text", "author").flatMap(m => Ok(m.asJson)) // TODO get request body
     case POST -> Root / "messages" / "pop" =>
