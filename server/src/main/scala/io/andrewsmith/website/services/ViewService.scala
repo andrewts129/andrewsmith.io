e154fbe04ff4f3eb5c20f96f1c29deafe8bcca35
Andrew Smith <andrewts129@gmail.com>
2020-01-20 13:05:38 -0500

    added links styling

--- server/src/main/scala/io/andrewsmith/website/services/ViewService.scala
+++ server/src/main/scala/io/andrewsmith/website/services/ViewService.scala
@@ -6,7 +6,6 @@ import org.http4s.HttpRoutes
 import org.http4s.dsl.io._
 import org.http4s.scalatags._
 
-// TODO add js and css
 object ViewService {
   val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
     case GET -> Root => Ok(Index.page)
