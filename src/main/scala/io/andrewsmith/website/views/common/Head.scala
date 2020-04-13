7763b1772824bc368793437d1576a0370563a668
Andrew Smith <andrewts129@gmail.com>
2020-04-13 17:32:31 -0400

    added viewport tag

--- src/main/scala/io/andrewsmith/website/views/common/Head.scala
+++ src/main/scala/io/andrewsmith/website/views/common/Head.scala
@@ -12,6 +12,7 @@ object Head {
     title(pageTitle),
     meta(name := "author", content := "Andrew Smith"),
     meta(name := "description", content := description),
+    meta(name := "viewport", content := "width=device-width, initial-scale=1"),
     link(rel := "icon", `type` := "image/png", href := "/assets/images/favicon.png"),
     additionalTags
   )
