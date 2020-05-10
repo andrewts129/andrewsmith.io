532c18c7c6f0837ca6e58ff77cf4b901807334f7
Andrew Smith <andrewts129@gmail.com>
2020-05-09 21:34:27 -0400

    fix js library loading

--- client/src/js/columbusBuildings.ts
+++ client/src/js/columbusBuildings.ts
@@ -1,76 +1,76 @@
-import * as mapboxgl from 'mapbox-gl';
+namespace ColumbusBuildings {
+    const main = (): void => {
+        if (!mapboxgl.supported()) {
+            window.alert("Sorry, your browser doesn't support the map on this page")
+        } else {
+            const tileServerUrl = 'http://andrewsmith.io:81'; // TODO
 
-const main = (): void => {
-    if (!mapboxgl.supported()) {
-        window.alert("Sorry, your browser doesn't support the map on this page")
-    } else {
-        const tileServerUrl = 'http://andrewsmith.io:81'; // TODO
-
-        const map = new mapboxgl.Map({
-            container: 'map',
-            center: [-82.9988, 39.9612],
-            zoom: 13,
-            minZoom: 11,
-            maxZoom: 16,
-            style: `${tileServerUrl}/styles/base/style.json`,
-            antialias: true,
-        });
-
-        map.on('load', () => {
-            map.addSource('buildings', {
-                type: 'vector',
-                tiles:[`${tileServerUrl}/data/buildings/{z}/{x}/{y}.pbf`],
-                minzoom: 11,
-                maxzoom: 15,
+            const map = new mapboxgl.Map({
+                container: 'map',
+                center: [-82.9988, 39.9612],
+                zoom: 13,
+                minZoom: 11,
+                maxZoom: 16,
+                style: `${tileServerUrl}/styles/base/style.json`,
+                antialias: true,
             });
 
-            map.addSource('columbus', {
-                type: 'vector',
-                tiles:[`${tileServerUrl}/data/columbus/{z}/{x}/{y}.pbf`]
-            });
+            map.on('load', () => {
+                map.addSource('buildings', {
+                    type: 'vector',
+                    tiles:[`${tileServerUrl}/data/buildings/{z}/{x}/{y}.pbf`],
+                    minzoom: 11,
+                    maxzoom: 15,
+                });
 
-            map.addControl(new mapboxgl.FullscreenControl());
+                map.addSource('columbus', {
+                    type: 'vector',
+                    tiles:[`${tileServerUrl}/data/columbus/{z}/{x}/{y}.pbf`]
+                });
 
-            const colors = ['#eaeae5', '#e41a1c', '#f24d0e', '#ff7f00', '#FFBF1A', '#ffff33', '#A6D73F', '#4daf4a', '#429781', '#377eb8', '#6866AE', '#984ea3'];
-            const stopYears = [0, 1800, 1825, 1850, 1875, 1900, 1925, 1950, 1975, 2000, 2025, 2050];
+                map.addControl(new mapboxgl.FullscreenControl());
 
-            map.addLayer({
-                id: 'buildings',
-                type: 'fill',
-                source: 'buildings',
-                'source-layer': 'buildings',
-                paint: {
-                    'fill-color': {
-                        'property': 'year_built',
-                        'stops': colors.map((color, index) => {
-                            return [stopYears[index], color]
-                        })
-                    }
-                },
-                layout: { // Sort so that older buildings are on top and undated buildings are at the back
-                    'fill-sort-key': ['case',
-                        ['==', ['get', 'year_built'], 0],
+                const colors = ['#eaeae5', '#e41a1c', '#f24d0e', '#ff7f00', '#FFBF1A', '#ffff33', '#A6D73F', '#4daf4a', '#429781', '#377eb8', '#6866AE', '#984ea3'];
+                const stopYears = [0, 1800, 1825, 1850, 1875, 1900, 1925, 1950, 1975, 2000, 2025, 2050];
+
+                map.addLayer({
+                    id: 'buildings',
+                    type: 'fill',
+                    source: 'buildings',
+                    'source-layer': 'buildings',
+                    paint: {
+                        'fill-color': {
+                            'property': 'year_built',
+                            'stops': colors.map((color, index) => {
+                                return [stopYears[index], color]
+                            })
+                        }
+                    },
+                    layout: { // Sort so that older buildings are on top and undated buildings are at the back
+                        'fill-sort-key': ['case',
+                            ['==', ['get', 'year_built'], 0],
                             -3000,
-                        // default
+                            // default
                             ['-', ['to-number', ['get', 'year_built']]]
-                    ] as unknown as number // It's a number, I promise
+                        ] as unknown as number // It's a number, I promise
+                    }
+                });
+            });
+
+            const collapseButton = document.getElementById('legend-source-collapse-button');
+            collapseButton.addEventListener('click', () => {
+                const source = document.getElementById('legend-source');
+                const arrow = collapseButton.firstElementChild;
+                if (!source.style.display || source.style.display === 'flex') {
+                    source.style.display = 'none';
+                    arrow.classList.add('rotated');
+                } else {
+                    source.style.display = 'flex';
+                    arrow.classList.remove('rotated');
                 }
             });
-        });
+        }
+    };
 
-        const collapseButton = document.getElementById('legend-source-collapse-button');
-        collapseButton.addEventListener('click', () => {
-            const source = document.getElementById('legend-source');
-            const arrow = collapseButton.firstElementChild;
-            if (!source.style.display || source.style.display === 'flex') {
-                source.style.display = 'none';
-                arrow.classList.add('rotated');
-            } else {
-                source.style.display = 'flex';
-                arrow.classList.remove('rotated');
-            }
-        });
-    }
-};
-
-window.onload = main;
+    window.onload = main;
+}
