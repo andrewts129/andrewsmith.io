c0108620b5aec4268a766313dd65bd8ddb79f73d
Andrew Smith <andrewts129@gmail.com>
2020-06-06 22:16:53 -0400

    removed unnecessary caching in bogosort database access (that didn't even work?)

--- server/src/main/scala/io/andrewsmith/website/db/BogoStat.scala
+++ server/src/main/scala/io/andrewsmith/website/db/BogoStat.scala
@@ -6,23 +6,12 @@ import doobie.implicits._
 case class BogoStat(key: String, value: Int)
 
 object BogoStat {
-  // So we don't need to hit the database every second for a number that changes once every few months
-  private var numCompletionsCache: Option[IO[Int]] = None
-
   def numCompletions: IO[Int] = {
-    numCompletionsCache match {
-      case Some(value) => value
-      case None =>
-        val sql = sql"SELECT value FROM BogoStat WHERE key = 'numCompletions';".query[Int].unique
-        val value = sql.transact(transactor)
-        numCompletionsCache = Some(value) // Cache this value
-        value
-    }
+    val sql = sql"SELECT value FROM BogoStat WHERE key = 'numCompletions';".query[Int].unique
+    sql.transact(transactor)
   }
 
   def IncrementNumCompletions: IO[Int] = {
-    numCompletionsCache = None // Invalidate cache
-
     val sql = for {
       _ <- sql"UPDATE BogoStat SET value = value + 1 WHERE key = 'numCompletions';".update.run
       select <- sql"SELECT value FROM BogoStat WHERE key = 'numCompletions';".query[Int].unique
