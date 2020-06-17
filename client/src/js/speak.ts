f073f9cde8546564bf8f6805009d42b021fbfbe8
Andrew Smith <andrewts129@gmail.com>
2020-06-16 22:10:26 -0400

    basic speaking

---
+++ client/src/js/speak.ts
@@ -0,0 +1,28 @@
+namespace Speak {
+    const textBox = document.getElementById('textBox') as HTMLInputElement
+    const submitButton = document.getElementById('submitButton') as HTMLInputElement;
+
+    const onSubmit = async (): Promise<void> => {
+        textBox.disabled = true;
+        submitButton.disabled = true;
+
+        const text = textBox.value;
+        const url = `https://voice.andrewsmith.io/${encodeURIComponent(text)}`;
+        const audio = new Audio(url);
+
+        audio.addEventListener('canplaythrough', () => {
+            audio.play();
+        });
+
+        audio.addEventListener('ended', () => {
+            textBox.disabled = false;
+            submitButton.disabled = false;
+        })
+    }
+
+    const main = (): void => {
+        submitButton.addEventListener('click', onSubmit)
+    };
+
+    window.onload = main;
+}
