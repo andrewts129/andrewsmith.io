88a308152eab687dae9a0d2b1f6d399f653981c1
Andrew Smith <andrewts129@gmail.com>
2021-01-25 20:36:44 -0500

    Avoid changing line break position

--- client/src/js/words.ts
+++ client/src/js/words.ts
@@ -11,15 +11,30 @@ namespace Words {
         let output = '';
         const wordsContainer = document.getElementById('wordsContainer');
 
+        const isWhitespace = (s: String) => s.trim() === '';
         const writeNextCharacter = (): void => {
             if (characterBuffer.length > 0) {
-                output = output + characterBuffer.shift();
+                output += characterBuffer.shift();
                 wordsContainer.innerText = output;
+
+                let restOfWordPlaceholderText = '';
+                for (const character of characterBuffer) {
+                    const x = isWhitespace(character)
+                    if (x) {
+                        break;
+                    } else {
+                        restOfWordPlaceholderText += character;
+                    }
+                }
+
+                const restOfWordPlaceholder = document.createElement('span');
+                restOfWordPlaceholder.innerText = restOfWordPlaceholderText;
+                wordsContainer.appendChild(restOfWordPlaceholder);
             }
         };
 
         writeNextCharacter();
-        setInterval(writeNextCharacter, 100);
+        setInterval(writeNextCharacter, 80);
     };
 
     window.onload = main;
