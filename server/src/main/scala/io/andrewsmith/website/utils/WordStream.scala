283ee2fbb4bc7b3db65117ae12c2eb9a2692074c
Andrew Smith <andrewts129@gmail.com>
2021-02-24 22:12:08 -0500

    Small refactoring in wordStream creation

--- server/src/main/scala/io/andrewsmith/website/utils/WordStream.scala
+++ server/src/main/scala/io/andrewsmith/website/utils/WordStream.scala
@@ -5,11 +5,11 @@ import cats.effect.{IO, Timer}
 import io.andrewsmith.markovs.Model
 
 import java.nio.file.Path
-import scala.concurrent.duration._
+import scala.concurrent.duration.DurationInt
 
 object WordStream {
   def wordStream(implicit timer: Timer[IO]): Stream[IO, String] = {
     val schemaPath = Path.of(sys.env.getOrElse("WORDS_SCHEMA_PATH", ".db/default.schema"))
-    Model.load(schemaPath).flatMap(Model.generateText).repeat.metered(3.seconds)
+    Model.load(schemaPath).flatMap(Model.generateText(_).repeat).metered(3.seconds)
   }
 }
